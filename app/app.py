dd0d4ebe851f3e4514c7a7ef2958d4d00ef49c00
Matt N <mattnappo@gmail.com>
2018-06-29 12:19:58 -0400

    Decrypt flask page works now

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, flash, request, redirect, url_for, session, logging
+from flask import Flask, render_template, flash, request, redirect, url_for, session, send_from_directory, logging
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'txt'])
 app = Flask(__name__, static_url_path='/static')
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-import random, settings
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, FileField
+from werkzeug.utils import secure_filename
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+import random, os
 from encrypt import Encrypt
+from decrypt import Decrypt
 class EncryptForm(Form):
     text = TextAreaField("", [validators.DataRequired()])
-@app.before_request
-def make_session_permanent():
-    session.permanent = True
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -20,13 +22,50 @@ def encrypt():
         file_name = str(random.random())[2:]
         Encrypt(
             text,
-            "static/img/" + file_name,
+            "static/encrypted/" + file_name,
             "png"
         )
         session['filename'] = file_name
         return render_template("encrypt.html", form=form)
     session['filename'] = ''
     return render_template("encrypt.html", form=form)
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/decrypt', methods=['GET', 'POST'])
+def upload_file():
+    session['filename'] = ""
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            PATH = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(PATH)
+            session['filename'] = PATH
+            text = ""
+            with open(Decrypt(PATH).NAME, "r") as f:
+                text = f.read()
+            print(text)
+            return render_template("decrypt.html", text=text)
+    else:
+        session['filename'] = ""
+    return render_template("decrypt.html")
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
     app.run(debug=True)
--- app/decrypt.py
+++ app/decrypt.py
@@ -1,8 +1,10 @@
 from PIL import Image
+import random
 class Decrypt():
     def __init__(self, image_location):
         self.binary = ""
         self.image = self.load(image_location)
+        self.NAME = ""
         self.decrypt()
     def load(self, image_location):
         image = Image.open(image_location, "r")
@@ -13,9 +15,11 @@ class Decrypt():
                 self.binary = self.binary + "1"
             elif pixel == (255, 255, 255):
                 self.binary = self.binary + "0"
-        self.export()
+        self.export(self.binary)
     def export(self, bits, encoding='utf-8', errors='surrogatepass'):
         n = int(bits, 2)
-        print(n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0')
-
+        NAME = str(random.random())[2:] + ".txt"
+        with open("static/decrypted/" + NAME, "w") as f:
+            f.write(n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0')
+            self.NAME = "static/decrypted/" + NAME
 # Decrypt("converted.png")
