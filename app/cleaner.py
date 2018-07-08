3f5073aa1a8c6f17dbc0625db823d7062fec865c
Matt N <mattnappo@gmail.com>
2018-07-08 15:08:39 -0400

    Did some work

--- app/cleaner.py
+++ app/cleaner.py
@@ -2,6 +2,10 @@ import os
 def clean(directory):
     directory = directory.lower()
     files = os.listdir(directory)
+    try:
+        files.remove("bootstrap")
+    except:
+        pass
     for x in range(len(files)):
         f = files[x]
         if os.path.isdir(directory + "/" + f) == True:
@@ -11,3 +15,9 @@ def clean(directory):
             os.remove(rem)
             print("Removed: " + rem)
     print("Done!")
+def new(dir_name):
+    with open("static/" + dir_name + "/.placeholder", "w+") as f:
+        pass
+dirs = ["encrypted", "decrypted", "uploads"]
+for dir in dirs:
+    new(dir)
